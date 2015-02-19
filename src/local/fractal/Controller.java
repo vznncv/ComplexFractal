4be0262e0f1dc77d2adfd93bbeb40084ed9b772e
Kochin Konstantin <Verzervul@gmail.com>
2015-02-19 22:30:04 +0300

    Add JUnit4 test for class Point2DTransformer.

--- src/local/fractal/Controller.java
+++ src/local/fractal/Controller.java
@@ -26,7 +26,7 @@ public class Controller {
     public void initialize() {
         // set canvas size as:
         // mainCanvas.width = root.width - wBorder
-        // mainCanvas.height =  root.height - hBorder;
+        // mainCanvas.height = root.height - hBorder;
         double wBorder = 40;
         double HBorder = 100;
         mainCanvas.widthProperty().bind(Bindings.subtract(root.widthProperty(), wBorder));
@@ -39,83 +39,8 @@ public class Controller {
      * Repaint canvas.
      */
     public void repaintCanvas() {
-        /*
 
-        Point2D a = new Point2D(1, 1);
-        Point2DTransformer  tr = new Point2DTransformer();
-
-        System.out.println("1) initial values:");
-        System.out.println("   point : " + a);
-        System.out.println("   matrix: " + tr);
-
-        // shift
-        tr.translation(1, 1);
-        System.out.println("s) after shift:");
-        System.out.println("   point : " + tr.apply(a));
-        System.out.println("   matrix: " + tr);
-
-        // rotate
-        tr.rotate(-Math.PI/2);
-        System.out.println("r) after rotate:");
-        System.out.println("   point : " + tr.apply(a));
-        System.out.println("   matrix: " + tr);
-
-        // scaling
-        tr.scale(2, 2);
-        System.out.println("s) after scaling:");
-        System.out.println("   point : " + tr.apply(a));
-        System.out.println("   matrix: " + tr);
-        */
-
-        /*
-        Point2D p = new Point2D(-1, 0);
-        MandelbrotSet ms = new MandelbrotSet();
-        System.out.println("Point: " + p + "; numIter: " + ms.numberIter(p.toComplex()));
-
-        // draw palette
-        IterativePaletteV1 pl = new IterativePaletteV1();
-        GraphicsContext gc = mainCanvas.getGraphicsContext2D();
-        double width = mainCanvas.getWidth();
-        double height = mainCanvas.getHeight();
-        PixelWriter pw = gc.getPixelWriter();
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                Color c = pl.numIterToColor(i + j);
-                pw.setColor(i, j, c);
-            }
-        }
-
-        // test stream
-        int w = 10;
-        int y = 0;
-        MandelbrotSet fr = new MandelbrotSet();
-        IterativePaletteV1 plFr = new IterativePaletteV1();
-
-        Object[] points = Stream.iterate(
-                new Point2D(0, y),
-                (prevPoint) -> new Point2D(prevPoint.getX() + 0.01, y))
-                .limit(w).toArray();
-
-        Point2D[] pn = (Point2D []) points;
-
-        // calculate Color for point
-        Stream<Color> colors = Arrays.stream(points)
-                .map((point) -> fr.numberIter(point.toComplex()))
-                .map((numIter) -> plFr.numIterToColor(numIter));
-
-        System.out.println("Points:");
-        Arrays.stream(points).forEach((point) -> System.out.print(point + "; "));
-        System.out.println();
-
-        System.out.println("Colors:");
-        colors.forEach((color) -> System.out.print(color + "; "));
-        System.out.println();*/
-
-        //fd = new ComplexFractalDrawer(mainCanvas, new MandelbrotSet());
         fd.getTransform().clear();
-        fd.getTransform().scale(1, 1);
         fd.draw();
-
-
     }
 }
