232d6f9a54dd4f2d40bd7be1d8a7d0c335cc64a4
Kochin Konstantin <Verzervul@gmail.com>
2015-03-30 19:51:43 +0300

    Split css styles.

--- src/frontend/Main.java
+++ src/frontend/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         // create the graph scene
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
         // create the scene
         Scene scene = new Scene(root);
         // add the scene to the stage
